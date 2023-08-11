45842670523591b9d390c8482d120da7eb4c5144
whatnext121 <adetounolamidejoseph@gmail.com>
2023-08-10 20:59:15 -0700

    5

--- 0x00-hello_world/5-print.c
+++ 0x00-hello_world/5-print.c
@@ -3,8 +3,8 @@
  * main - A program that prints a line with puts function
  * Return: 0 (Success)
  */
-int main(void)
+int main[void]
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+  puts('\'"Programming is like building a multilingual puzzle);
+  return (0);
 }
