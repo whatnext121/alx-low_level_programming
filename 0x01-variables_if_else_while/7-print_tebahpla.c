44fa49c75ff09c1be4a1cdfd1de7a74bbbc00c2c
whatnext121 <adetounolamidejoseph@gmail.com>
2023-08-11 21:24:39 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * Main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int low;
+
+        for (low = 'z'; low <= 'a'; low++)
+        putchar(low);
+        putchar('\n');
+
+        return (0);
+
+}
