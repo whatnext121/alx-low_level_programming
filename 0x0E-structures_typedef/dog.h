fdf4b2a511294aa8062f2b9e12587c0550414958
whatnext121 <adetounolamidejoseph@gmail.com>
2023-09-12 13:40:53 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct dog info
+ *
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: struct of dog
+ */
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+};
+#endif
+
